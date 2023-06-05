bdcc74c2481f136b77c223fdea73f5ddbdbd81b4
Yusuf KORUCU <yusuf.korucu@bilgeadam.com>
2023-06-05 14:15:00 +0300

    #publisher subscriber app created

---
+++ Redis.PubSubApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//korucu
+
+using StackExchange.Redis;
+
+var redis = await ConnectionMultiplexer.ConnectAsync("localhost:1923");
+
+var subscriber = redis.GetSubscriber();
+
+while (true)
+{
+    Console.Write("Message: ");
+    string message = Console.ReadLine();
+    await subscriber.PublishAsync("Korucu", message);
+}
---
+++ Redis.PubSubSubscriberApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+var redis = await ConnectionMultiplexer.ConnectAsync("localhost:1923");
+
+var subscriber = redis.GetSubscriber();
+
+subscriber.Subscribe("Korucu", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+
+Console.Read();
