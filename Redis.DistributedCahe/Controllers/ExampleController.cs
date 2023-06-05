c0616f32dcb9be6ef0b17da509e027ee4b2e4ae3
Yusuf KORUCU <yusuf.korucu@bilgeadam.com>
2023-06-05 13:50:24 +0300

    Add project files.

---
+++ Redis.DistributedCahe/Controllers/ExampleController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace Redis.DistributedCahe.Controllers
+{
+    [Route("v1/api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        readonly IDistributedCache _distributedCache;
+
+        public ExampleController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet("Set")]
+        public async Task<IActionResult> Set(string name, string surname)
+        {
+            await _distributedCache.SetStringAsync("name", name, options: new DistributedCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(1),
+                SlidingExpiration = TimeSpan.FromSeconds(15)
+            });
+
+            await _distributedCache.SetAsync("surname", Encoding.UTF8.GetBytes(surname));
+
+            return Ok();
+        }
+
+        [HttpGet("Get")]
+
+        public async Task<IActionResult> Get()
+        {
+            var name = await _distributedCache.GetStringAsync("name");
+
+            var surnameBinary = await _distributedCache.GetAsync("surname");
+
+            var surname = Encoding.UTF8.GetString(surnameBinary);
+
+            return Ok(new
+            {
+                name,
+                surname
+            });
+        }
+    }
+}
---
+++ Redis.InMemoryCache/Controllers/ExampleController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Redis.InMemoryCache.Controllers
+{
+    [Route("v1/api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ExampleController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+
+        [HttpGet("Set/{name}")]
+        public void Set(string name)
+        {
+            _memoryCache.Set("name", name);
+
+        }
+
+        [HttpGet("Get")]
+        public string Get()
+        {
+            if (_memoryCache.TryGetValue<string>("name", out string name))
+                return name;
+
+            return "";
+        }
+        [HttpGet("SetDate")]
+
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now,options:new MemoryCacheEntryOptions
+            {
+                AbsoluteExpiration=DateTimeOffset.Now.AddSeconds(30),
+                SlidingExpiration=TimeSpan.FromSeconds(5),
+            });
+        }
+
+        [HttpGet("GetDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+
+    }
+}
